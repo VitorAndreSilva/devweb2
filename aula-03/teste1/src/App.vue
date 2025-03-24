b7271537881df9283a8c6be5a53435e9069024d0
VitorAndreSilva <alunovitorandre@gmail.com>
2025-03-24 11:56:06 -0300

    FEAT: class 3

---
+++ aula-03/teste1/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+const contador = ref(0);
+
+function incrementar() {
+  contador.value++
+}
+function decrementar() {
+  contador.value--
+}
+
+</script>
+
+<template>
+  <h1>Olá, mundo</h1>
+  <div v-if="contador<=46">
+    <p>Contador: {{ contador }}</p>
+    <button @click="incrementar">+</button>
+    <button @click="decrementar">-</button>
+  </div>
+  <div v-else="contador=46">
+    <p>Contador: {{ contador }}</p>
+    <p>Impossível incrementar</p>
+    <button @click="decrementar">-</button>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ aula-03/teste5/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Olá, mundo</h1>
+</template>
+
+<style scoped>
+
+</style>
