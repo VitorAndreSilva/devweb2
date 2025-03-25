03237853ec8c85f46b76f18ee666f1157a25e2b2
VitorAndreSilva <alunovitorandre@gmail.com>
2025-03-25 09:27:19 -0300

    FEAT: 04 class

---
+++ aula-04/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const contador = ref(0);
+</script>
+
+<template>
+  <div v-if="contador>=0 & contador<44">
+    <h1>Contador de Aniversário</h1>
+    <p>{{ contador }}</p>
+    <button @click="contador++">+</button>
+    <button @click="contador--">-</button>
+  </div>
+  <div v-else-if="contador<0">
+    <h1>Contador de Aniversário</h1>
+    <p>O contador não pode ser menor que 0</p>
+    <button @click="contador++">+</button>
+  </div>
+  <div v-else-if="contador=44" class="parabens">
+    <h1>Parabéns, mamãe!</h1>
+    <p>{{ contador }} 🥳🥳🥳</p>
+    <p>Deus abençoe muito a sua vida! Seu mini programador ama muito você! ❤️</p>
+    <button @click="contador--">-</button>
+  </div>
+</template>
+
+<style scoped>
+  .parabens h1 {
+    font-family: 'Times New Roman', Times, serif;
+    color: rgb(219, 43, 43);
+  }
+  button {
+    height: 2.5vw;
+    width: 2.5vw;
+  }
+</style>
